ee3dd1bc15ebf094ddad73841344a936a6677541
david-marcoss <ddragondavi@gmail.com>
2025-08-30 15:33:04 -0300

    Initial commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <h1 className="text-white">Money Converter</h1>
+    </div>
+  );
+}
