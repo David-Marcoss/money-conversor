f4898f5b2a94d486ad7c8c50b54045ba0588c7a5
david-marcoss <ddragondavi@gmail.com>
2025-08-30 15:58:12 -0300

    config: config shadcn lib

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 export function HomePage() {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-white">Money Converter</h1>
+      <h1>Money Converter</h1>
     </div>
   );
 }
